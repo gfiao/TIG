04d0c91469469f0170904a8e062a1b7361b9f5b2
Gui <g.fiao@campus.fct.unl.pt>
2015-05-06 17:17:56 +0100

    script para popular a BD falta criar mais tuplos

---
+++ bd.sql
@@ -0,0 +1,11 @@
+insert into cities values ('Lisboa');
+insert into cities values ('Almada');
+
+insert into types values ('Restaurante');
+insert into types values ('Monumento');
+insert into types values ('Museu');
+insert into types values ('Hotel');
+insert into types values ('Hostel');
+
+insert into markers values (1, 'Monumento', 'Lisboa', 'Mosteiro dos Jeronimos', 38.697937, -9.206243, 10, 10, 18, 
+											'O Mosteiro dos Jeronimos foi declarado Monumento Nacional em 1907.');
